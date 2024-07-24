3f4b20d812920d0fcd3f8793d3a87368cbbb8651
areeb1501 <64038736+areeb1501@users.noreply.github.com>
2024-07-24 16:38:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-
-
 import streamlit as st
-def main():
-    st.title("Hello, World Areeb!")
-    st.write("This is a simple Streamlit application.")
 
-if __name__ == "__main__":
-    main()
-
-
+st.title("Hello, World Areeb!")
+st.write("This is a simple Streamlit application.")
